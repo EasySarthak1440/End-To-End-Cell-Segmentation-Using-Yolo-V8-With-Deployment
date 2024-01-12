81c4f0f52f0d43da6e0290b16456bd6f80d37c12
Sarthak Kelkar <kelkarsarthak2002@gmail.com>
2024-01-12 07:36:35 +0530

    update setup.py 1

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cellSegmentation',
+    version= '0.0.0',
+    author= 'Sarthak Santosh Kelkar',
+    author_email= 'kelkarsarthak2002@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
